560a4477627c4746fcc8a0aa1bce0d03e45e2b24
Osama Abaali <40522456+OAAbaali2896@users.noreply.github.com>
2019-05-27 19:54:06 -0400

    Update src.c

--- Mark-1/C/src.c
+++ Mark-1/C/src.c
@@ -7,262 +7,84 @@ void delay(int count)
 int main (void) 
 {
 	int a;
-	int count_f=1;//frequency mode
-	int count_m=1; //mode
-	int mask_val=       0x00000800; //bit 11 in 32-bit
-	int mask_val2=      0x00001000; //bit 12 in 32-bit
+	int count_f=1;//frequency mode to change the speed of 8 LEDs
+	int count_m=1; //mode to change the pattern of 8 LEDs
+	int mask_val= 1<<11; //KEY1
+	int mask_val2= 1<<12; //Key2
+	int frequency;
 	int on_val = 0;
+	
 	LPC_GPIO2->FIODIR = 0x000000FF; // P2.0, P2.1,...P2.7 pins/LEDs on port2 set as output
 	//urn on/off the LED driven by P2.0; 
 	while(1){
-		/*if((LPC_GPIO2->FIOPIN & mask_val)==0)
-		{ 
-			//LPC_GPIO2->FIODIR |= 1;  
+		if((LPC_GPIO2->FIOPIN & mask_val)==0) //checks if KEY1 is pressed
+		{   
 			count_m++;
 			if(count_m>4)
-			{
 				count_m=1;
+		}
+		if((LPC_GPIO2->FIOPIN & mask_val2)==0) //checks if KEY2 is pressed
+		{   
+			count_f++;
+			if(count_f>3)
+				count_f=1;
+		}
+		
+		if(count_f==1)
+			frequency=12000000;
+		if(count_f==2)
+			frequency=3000000;
+		if(count_f==3)
+			frequency=24000000;
+		
+		if(count_m==1)
+		{
+			a=7;
+			while(a>=0){
+				LPC_GPIO2->FIOPIN |= 1 << a; // make P2.0 high 
+				delay(frequency); // delay of about 1 s implemented in software;
+				LPC_GPIO2->FIOPIN &= ~1 << a; // make P2.0 low
+				delay(frequency);
+				a--;
 			}
 		}
-		//else {LPC_GPIO2->FIODIR &= ~1;}
-			*/
-		switch(count_m)
+		if(count_m==2)
 		{
-			case 1:
-							if((LPC_GPIO2->FIOPIN & mask_val)==0)  
-							{
-								count_m=2;
-								break;
-							}
-							switch(count_f)
-							{
-								case 1:
-									if((LPC_GPIO2->FIOPIN & mask_va2)==0)  
-									{
-										count_f=2;
-										break;
-									}
-									a=7;
-									while(a>=0){
-										LPC_GPIO2->FIOPIN |= 1 << a; // make P2.0 high 
-										delay(12000000); // delay of about 1 s implemented in software;
-										LPC_GPIO2->FIOPIN &= ~1 << a; // make P2.0 low
-										delay(12000000);
-										a--;
-									}
-									break;
-								case 2:
-									if((LPC_GPIO2->FIOPIN & mask_va2)==0)  
-									{
-										count_f=3;
-										break;
-									}
-									a=7;
-									while(a>=0){
-										LPC_GPIO2->FIOPIN |= 1 << a; // make P2.0 high 
-										delay(3000000); // delay of about 1 s implemented in software;
-										LPC_GPIO2->FIOPIN &= ~1 << a; // make P2.0 low
-										delay(3000000);
-										a--;
-									}
-									break;
-								case 3:
-									if((LPC_GPIO2->FIOPIN & mask_va2)==0)  
-									{
-										count_f=1;
-										break;
-									}
-									a=7;
-									while(a>=0){
-										LPC_GPIO2->FIOPIN |= 1 << a; // make P2.0 high 
-										delay(24000000); // delay of about 1 s implemented in software;
-										LPC_GPIO2->FIOPIN &= ~1 << a; // make P2.0 low
-										delay(24000000);
-										a--;
-									}
-									break;
-							}	
-							break;
-										
-			case 2:
-							if((LPC_GPIO2->FIOPIN & mask_val)==0)  
-							{
-								count_m=3;
-								break;
-							}
-							switch(count_f)
-							{
-								case 1:
-								if((LPC_GPIO2->FIOPIN & mask_va2)==0)  
-									{
-										count_f=2;
-										break;
-									}							
-									a=0;		
-									while(a<=7){
-									LPC_GPIO2->FIOPIN |= 1 << a; // make P2.0 high 
-									delay(12000000); // delay of about 1 s implemented in software;
-									LPC_GPIO2->FIOPIN &= ~1 << a; // make P2.0 low
-									delay(12000000);
-									a++;
-									}
-									break;
-								case 2:
-									if((LPC_GPIO2->FIOPIN & mask_va2)==0)  
-									{
-										count_f=3;
-										break;
-									}							
-									a=0;		
-									while(a<=7){
-									LPC_GPIO2->FIOPIN |= 1 << a; // make P2.0 high 
-									delay(3000000); // delay of about 1 s implemented in software;
-									LPC_GPIO2->FIOPIN &= ~1 << a; // make P2.0 low
-									delay(3000000);
-									a++;
-									}
-									break;
-								case 3:
-									if((LPC_GPIO2->FIOPIN & mask_va2)==0)  
-									{
-										count_f=1;
-										break;
-									}							
-									a=0;		
-									while(a<=7){
-									LPC_GPIO2->FIOPIN |= 1 << a; // make P2.0 high 
-									delay(24000000); // delay of about 1 s implemented in software;
-									LPC_GPIO2->FIOPIN &= ~1 << a; // make P2.0 low
-									delay(24000000);
-									a++;
-									}
-									break;
-								}
-							break;	
-									
-			case 3:
-							if((LPC_GPIO2->FIOPIN & mask_val)==0)  
-							{
-								count_m=4;
-								break;
-							}
-							switch(count_f)
-							{
-								case 1:
-										if((LPC_GPIO2->FIOPIN & mask_va2)==0)  
-										{
-											count_f=2;
-											break;
-										}
-										a=7;
-										while(a>0){
-											LPC_GPIO2->FIOPIN |= 1 << a; // make P2.0 high 
-											delay(12000000); // delay of about 1 s implemented in software;
-											LPC_GPIO2->FIOPIN &= ~1 << a; // make P2.0 low
-											delay(12000000);
-											a--;
-										}
-										a=0;
-										while(a<7){
-											LPC_GPIO2->FIOPIN |= 1 << a; // make P2.0 high 
-											delay(12000000); // delay of about 1 s implemented in software;
-											LPC_GPIO2->FIOPIN &= ~1 << a; // make P2.0 low
-											delay(12000000);
-											a++;
-										}
-										break;
-								case 2:
-									if((LPC_GPIO2->FIOPIN & mask_va2)==0)  
-										{
-											count_f=3;
-											break;
-										}
-										a=7;
-										while(a>0){
-											LPC_GPIO2->FIOPIN |= 1 << a; // make P2.0 high 
-											delay(3000000); // delay of about 1 s implemented in software;
-											LPC_GPIO2->FIOPIN &= ~1 << a; // make P2.0 low
-											delay(3000000);
-											a--;
-										}
-										a=0;
-										while(a<7){
-											LPC_GPIO2->FIOPIN |= 1 << a; // make P2.0 high 
-											delay(3000000); // delay of about 1 s implemented in software;
-											LPC_GPIO2->FIOPIN &= ~1 << a; // make P2.0 low
-											delay(3000000);
-											a++;
-										}
-										break;
-								case 3:
-									if((LPC_GPIO2->FIOPIN & mask_va2)==0)  
-										{
-											count_f=2;
-											break;
-										}
-										a=7;
-										while(a>0){
-											LPC_GPIO2->FIOPIN |= 1 << a; // make P2.0 high 
-											delay(24000000); // delay of about 1 s implemented in software;
-											LPC_GPIO2->FIOPIN &= ~1 << a; // make P2.0 low
-											delay(24000000);
-											a--;
-										}
-										a=0;
-										while(a<7){
-											LPC_GPIO2->FIOPIN |= 1 << a; // make P2.0 high 
-											delay(24000000); // delay of about 1 s implemented in software;
-											LPC_GPIO2->FIOPIN &= ~1 << a; // make P2.0 low
-											delay(24000000);
-											a++;
-										}
-										break;
-									}
-							break;
-												
-			case 4:
-							if((LPC_GPIO2->FIOPIN & mask_val)==0)  
-							{
-								count_m=1;
-								break;
-							}
-							switch(count_f)
-							{
-								case 1:
-									if((LPC_GPIO2->FIOPIN & mask_va2)==0)  
-									{
-										count_f=2;
-										break;
-									}
-									LPC_GPIO2->FIOPIN |= 0x000000FF; // make P2.0 high 
-									delay(12000000); // delay of about 1 s implemented in software;
-									LPC_GPIO2->FIOPIN &=~0x000000FF; // make P2.0 low
-									delay(12000000);
-									break;
-								case 2:
-									if((LPC_GPIO2->FIOPIN & mask_va2)==0)  
-									{
-										count_f=3;
-										break;
-									}
-									LPC_GPIO2->FIOPIN |= 0x000000FF; // make P2.0 high 
-									delay(3000000); // delay of about 1 s implemented in software;
-									LPC_GPIO2->FIOPIN &=~0x000000FF; // make P2.0 low
-									delay(3000000);
-									break;
-								case 3:
-									if((LPC_GPIO2->FIOPIN & mask_va2)==0)  
-									{
-										count_f=1;
-										break;
-									}
-									LPC_GPIO2->FIOPIN |= 0x000000FF; // make P2.0 high 
-									delay(24000000); // delay of about 1 s implemented in software;
-									LPC_GPIO2->FIOPIN &=~0x000000FF; // make P2.0 low
-									delay(24000000);
-									break;
-								}
-							break;
-	}
+			a=0;		
+			while(a<=7){
+				LPC_GPIO2->FIOPIN |= 1 << a; // make P2.0 high 
+				delay(frequency); // delay of about 1 s implemented in software;
+				LPC_GPIO2->FIOPIN &= ~1 << a; // make P2.0 low
+				delay(frequency);
+				a++;
+			}
+		}
+		if(count_m==3)
+		{
+			a=7;
+			while(a>0){
+				LPC_GPIO2->FIOPIN |= 1 << a; // make P2.0 high 
+				delay(frequency); // delay of about 1 s implemented in software;
+				LPC_GPIO2->FIOPIN &= ~1 << a; // make P2.0 low
+				delay(frequency);
+				a--;
+			}
+			a=0;
+			while(a<7){
+				LPC_GPIO2->FIOPIN |= 1 << a; // make P2.0 high 
+				delay(frequency); // delay of about 1 s implemented in software;
+				LPC_GPIO2->FIOPIN &= ~1 << a; // make P2.0 low
+				delay(frequency);
+				a++;
+			
+			}
+		}
+		if(count_m==4)
+		{
+			LPC_GPIO2->FIOPIN |= 0x000000FF; // make P2.0 high 
+			delay(frequency); // delay of about 1 s implemented in software;
+			LPC_GPIO2->FIOPIN &=~0x000000FF; // make P2.0 low
+			delay(frequency);
+		}
 	}
 }
